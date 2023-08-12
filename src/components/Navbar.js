9f56bdf7bb305b27a49d60c181e4e42b66c75ead
Rahul Kumar Nitp <rahulk.ug21.cs@nitp.ac.in>
2023-08-13 00:06:35 +0530

    creating navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar(){
+    return(
+        <div className="navbar container">
+            <a href="#!" className="logo">F<span>oo</span>dieshub</a>
+            <div className="nav-links">
+                <a href="#!">Home</a>
+                <a href="#!">Recipes</a>
+                <a href="#!">Settings</a>
+            </div>
+        </div>
+    );
+}
